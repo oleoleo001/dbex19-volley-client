c03b90d17c240d3a965b436e75774dfdf2528adc
oleoleo001 <lolands@naver.com>
2021-11-26 17:10:11 +0900

    🏗️ 싱글톤 패턴 적용

--- app/src/main/java/kr/ac/kumoh/s20181382/gunplaapplication/MainActivity.kt
+++ app/src/main/java/kr/ac/kumoh/s20181382/gunplaapplication/MainActivity.kt
@@ -3,14 +3,54 @@ package kr.ac.kumoh.s20181382.gunplaapplication
 import android.app.Activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.RequestQueue
+import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.Volley
 import kr.ac.kumoh.s20181382.gunplaapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
+    companion object {
+        const val QUEUE_TAG = "VolleyRequest"
+    }
+    private lateinit var mQueue: RequestQueue
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        mQueue = VolleyRequest.getInstance(this.applicationContext).requestQueue
+
+        requestMechanic()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mQueue.cancelAll(QUEUE_TAG)
+    }
+
+    private fun requestMechanic() {
+        // NOTE: 서버 주소는 본인의 서버 주소 사용할 것
+        val url = "https://expressforvolley-ntofl.run.goorm.io/gunpladb/mechanic"
+
+        val request = JsonArrayRequest(
+            Request.Method.GET,
+            url,
+            null,
+            {
+                binding.result.text = it.toString()
+            },
+            {
+                Toast.makeText(this, it.toString(), Toast.LENGTH_LONG).show()
+                binding.result.text = it.toString()
+            }
+        )
+
+        request.tag = QUEUE_TAG
+        mQueue.add(request)
     }
 }
